29aec4556cb23727b885ba8d52c896218f3fa743
Vikosh18 <158554292+Vikosh18@users.noreply.github.com>
2024-04-05 09:26:14 +0300

    Update 1.py

--- 8lba/1.py
+++ 8lba/1.py
@@ -1,14 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-function Hello() {
-  return <div>Hello, World!</div>;
-}
-
-
-ReactDOM.render(
-  <Hello />,
-  document.getElementById('root')
-);
 
