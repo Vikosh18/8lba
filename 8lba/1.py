be252caac4bf7df3af10b74f01260bc49a6278cc
Vikosh18 <158554292+Vikosh18@users.noreply.github.com>
2024-04-05 09:24:46 +0300

    Update 1.py

--- 8lba/1.py
+++ 8lba/1.py
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Створення компоненту
+
 function Hello() {
   return <div>Hello, World!</div>;
 }
 
-// Рендеринг компоненту в DOM
+
 ReactDOM.render(
   <Hello />,
   document.getElementById('root')
